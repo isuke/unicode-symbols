7a8a1d4783f44844affd0812ef53afad58202e02
isuke <isuke770@gmail.com>
2019-11-21 19:46:57 +0900

    feat: add an animation sample

--- src/components/AnimationList.vue
+++ src/components/AnimationList.vue
@@ -14,6 +14,7 @@ export default
     animations: [
       { wide: false, strs: ["", "▏", "▎", "▍", "▌", "▋", "▊", "▉"] }
       { wide: false, strs: ["", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"] }
+      { wide: false, strs: ["▝", "▗", "▖", "▘"] }
       { wide: false, strs: ["╀", "┾", "╁", "┽"] }
       { wide: false, strs: ["▸▹▹▹", "▹▸▹▹", "▹▹▸▹", "▹▹▹▸"] }
       { wide: false, strs: ["☰", "☱", "☲", "☴"] }
